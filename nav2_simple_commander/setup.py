187b36015b39980b65d054a1e9f66f86a8999b03
pierre <pierre.inherent@gmail.com>
2022-03-28 17:48:48 +0800

    Revert "files added to allow multi-robot simulation in the warehouse environment"
    This reverts commit d6d32fc2dc869ec3e7ab118826bf0dae1fe9b8e9.

--- nav2_simple_commander/setup.py
+++ nav2_simple_commander/setup.py
@@ -31,7 +31,6 @@ setup(
                 'demo_picking = nav2_simple_commander.demo_picking:main',
                 'demo_inspection = nav2_simple_commander.demo_inspection:main',
                 'demo_security = nav2_simple_commander.demo_security:main',
-                'multi_picking = nav2_simple_commander.multi_picking:main',
         ],
     },
 )
